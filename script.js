b716f284e696809b16d2575e632ebb91685e36b7
Al_Iraq1 <152442043+q0so@users.noreply.github.com>
2025-02-19 21:05:00 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Loading 
+
+
+window.addEventListener("load", () => {
+    const loadingScreen = document.getElementById("loading-screen");
+    loadingScreen.style.display = "none";
+
+    const content = document.getElementById("content");
+    content.style.display = "block";
+});
